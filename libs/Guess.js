f16b881ad6467b69389e4db9d1727f69641177c5
mersanuzun <mersanuzun@gmail.com>
2015-09-29 00:11:44 +0300

    init

---
+++ libs/Guess.js
@@ -0,0 +1,9 @@
+function Guess(color1, color2, color3, color4){
+    this.color1 = color1;
+    this.color2 = color2;
+    this.color3 = color3;
+    this.color4 = color4;
+    this.wrongColorNumber;
+    this.wrongPlaceColorNumber;
+    this.correctColorNumber;
+}
